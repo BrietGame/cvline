238ceb454599b439b0bd79dfe9fba73ca0a48f40
Pinto Jody <pintojody20@gmail.com>
2022-02-15 15:42:17 +0100

    start ajax generatecv :rocket:

--- inc/func.php
+++ inc/func.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function debug($tableau)
 {
     echo '<pre style="height:400px;overflow-y: scroll;font-size: .7rem;padding: .6rem;font-family: Consolas, Monospace;background-color: #000;color:#fff;">';
@@ -9,6 +7,46 @@ function debug($tableau)
     echo '</pre>';
 }
 
+function cleanXss($key): string
+{
+    return trim(strip_tags($_POST[$key]));
+}
+
+function textValidation(array $errors,string $value,string $key,int $min = 3,int $max = 50):array
+{
+    if(!empty($value)) {
+        if(mb_strlen($value) < $min) {
+            $errors[$key] = 'Min '.$min.' caractères';
+        } elseif (mb_strlen($value) > $max) {
+            $errors[$key] = 'Max '.$max.' caractères';
+        }
+    } else {
+        $errors[$key] = 'Veuillez renseigner ce champ.';
+    }
+    return $errors;
+}
+
+function emailValidation($errors,$email,$key)
+{
+    if(!empty($email)) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[$key] = 'Veuillez renseigner un email valide';
+        }
+    } else {
+        $errors[$key] = 'Veuillez renseigner un email';
+    }
+    return $errors;
+}
+
+function showJson($data){
+    header("Content-type: application/json");
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+    if ($json){
+        die($json);
+    }else{
+        die('error in Json encoding');
+    }
+}
 
 function asset() {
     return get_template_directory_uri() . '/asset/' ;
--- template-generatecv.php
+++ template-generatecv.php
@@ -7,6 +7,24 @@ get_header();
         <h1>GenerationCv</h1>
     </div>
 
+<form method="POST" id="global_cv">
+    <input type="text" id="js_surname" name="surname" placeholder="Doe">
+    <span class="error" id="error_surname"></span>
+
+    <input type="text" id="js_name" name="name" placeholder="John">
+    <span class="error" id="error_name"></span>
+
+    <input type="text" id="js_email" name="email" placeholder="exemple@gmail.com">
+    <span class="error" id="error_mail"></span>
+
+    <input type="text" id="js_adress" name="adress" placeholder="8 rue exemple">
+    <input type="number" id="js_postal" name="postal" placeholder="76100">
+    <input type="text" id="js_city" name="city" placeholder="Rouen">
+
+    <input type="submit" name="submitted" id="js_submitted_global">
+
+</form>
+
 
 
 <?php
