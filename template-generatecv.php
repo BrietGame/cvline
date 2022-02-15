18c15e7fec020dfb4678110fff054848d8d5b167
dormandluc <luc.dormand@hotmail.fr>
2022-02-15 17:07:16 +0100

    :construction:Ajax Exp CV

--- template-generatecv.php
+++ template-generatecv.php
@@ -6,7 +6,7 @@ get_header();
     <div>
         <h1>GenerationCv</h1>
     </div>
-
+<section>
 <form method="POST" id="global_cv">
     <input type="text" id="js_surname" name="surname" placeholder="Doe">
     <span class="error" id="error_surname"></span>
@@ -24,8 +24,30 @@ get_header();
     <input type="submit" name="submitted" id="js_submitted_global">
 
 </form>
+</section>
+<section>
+    <form method="POST" id="experience_cv">
+        <input type="date" id="js_predate" name="predate" placeholder="">
+        <span class="error" id="error_predate"></span>
 
+        <input type="date" id="js_lastdate" name="lastdate" placeholder="">
+        <span class="error" id="error_lastdate"></span>
 
+        <input type="text" id="js_post_name" name="postname" placeholder="*Boulanger">
+        <span class="error" id="error_post_name"></span>
+
+        <input type="text" id="js_entreprise_name" name="entreprisename" placeholder="Elsi & Franck">
+        <span class="error" id="error_entreprise_name"></span>
+
+        <input type="text" id="js_post_place" name="postplace" placeholder="Cherbourg">
+        <span class="error" id="error_post_place"></span>
+
+        <input type="text" id="js_post_description" name="postdescription" placeholder="Je sais faire du pain">
+        <span class="error" id="error_post_description"></span>
+
+        <input type="submit" name="submitted" id="js_submitted_experience">
+    </form>
+</section>
 
 <?php
 get_footer();
