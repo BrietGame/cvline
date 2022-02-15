1f18383d0f0d7e9a59ba6b7e2e05c49f6a786544
brietalexis <alexis.briet2003@gmail.com>
2022-02-15 15:07:39 +0100

    :rocket: Login Form

--- inc/general.php
+++ inc/general.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Projet CVtheques functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package Projet_CVtheques
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
     // Replace the version number of the theme on each release.
-    define( '_S_VERSION', '1.0.0' );
+    define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function projet_cvtheques_setup() {
+function projet_cvtheques_setup()
+{
     /*
         * Make theme available for translation.
         * Translations can be filed in the /languages/ directory.
         * If you're building a theme based on Projet CVtheques, use a find and replace
         * to change 'projet-cvtheques' to the name of your theme in all the template files.
         */
-    load_theme_textdomain( 'projet-cvtheques', get_template_directory() . '/languages' );
+    load_theme_textdomain('projet-cvtheques', get_template_directory() . '/languages');
 
     // Add default posts and comments RSS feed links to head.
-    add_theme_support( 'automatic-feed-links' );
+    add_theme_support('automatic-feed-links');
 
     /*
         * Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function projet_cvtheques_setup() {
         * hard-coded <title> tag in the document head, and expect WordPress to
         * provide it for us.
         */
-    add_theme_support( 'title-tag' );
+    add_theme_support('title-tag');
 
     /*
         * Enable support for Post Thumbnails on posts and pages.
         *
         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
         */
-    add_theme_support( 'post-thumbnails' );
+    add_theme_support('post-thumbnails');
 
     // This theme uses wp_nav_menu() in one location.
     register_nav_menus(
         array(
-            'menu-1' => esc_html__( 'Primary', 'projet-cvtheques' ),
+            'menu-1' => esc_html__('Primary', 'projet-cvtheques'),
         )
     );
 
@@ -83,7 +85,7 @@ function projet_cvtheques_setup() {
     );
 
     // Add theme support for selective refresh for widgets.
-    add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support('customize-selective-refresh-widgets');
 
     /**
      * Add support for core custom logo.
@@ -100,7 +102,7 @@ function projet_cvtheques_setup() {
         )
     );
 }
-add_action( 'after_setup_theme', 'projet_cvtheques_setup' );
+add_action('after_setup_theme', 'projet_cvtheques_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'projet_cvtheques_setup' );
  *
  * @global int $content_width
  */
-function projet_cvtheques_content_width() {
-    $GLOBALS['content_width'] = apply_filters( 'projet_cvtheques_content_width', 640 );
+function projet_cvtheques_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('projet_cvtheques_content_width', 640);
 }
-add_action( 'after_setup_theme', 'projet_cvtheques_content_width', 0 );
+add_action('after_setup_theme', 'projet_cvtheques_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function projet_cvtheques_widgets_init() {
+function projet_cvtheques_widgets_init()
+{
     register_sidebar(
         array(
-            'name'          => esc_html__( 'Sidebar', 'projet-cvtheques' ),
+            'name'          => esc_html__('Sidebar', 'projet-cvtheques'),
             'id'            => 'sidebar-1',
-            'description'   => esc_html__( 'Add widgets here.', 'projet-cvtheques' ),
+            'description'   => esc_html__('Add widgets here.', 'projet-cvtheques'),
             'before_widget' => '<section id="%1$s" class="widget %2$s">',
             'after_widget'  => '</section>',
             'before_title'  => '<h2 class="widget-title">',
@@ -132,20 +136,39 @@ function projet_cvtheques_widgets_init() {
         )
     );
 }
-add_action( 'widgets_init', 'projet_cvtheques_widgets_init' );
+add_action('widgets_init', 'projet_cvtheques_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function projet_cvtheques_scripts() {
-    wp_enqueue_style( 'projet-cvtheques-style', get_stylesheet_uri(), array(), _S_VERSION );
-    wp_style_add_data( 'projet-cvtheques-style', 'rtl', 'replace' );
+function projet_cvtheques_scripts()
+{
+    wp_enqueue_style('projet-cvtheques-style', get_stylesheet_uri(), array(), _S_VERSION);
+    wp_style_add_data('projet-cvtheques-style', 'rtl', 'replace');
 
-    wp_enqueue_script( 'projet-cvtheques-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script('projet-cvtheques-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
     }
 }
-add_action( 'wp_enqueue_scripts', 'projet_cvtheques_scripts' );
+add_action('wp_enqueue_scripts', 'projet_cvtheques_scripts');
 
+function my_login_logo()
+{ ?>
+    <style type="text/css">
+        #login h1 a,
+        .login h1 a {
+            background-image: url(<?php echo get_template_directory_uri(); ?>/asset/img/logoCVline.svg);
+            width: 100%;
+            background-repeat: no-repeat;
+        }
+    </style>
+<?php }
+add_action('login_enqueue_scripts', 'my_login_logo');
+
+function my_custom_login()
+{
+    echo '<link rel="stylesheet" type="text/css" href="' . get_template_directory_uri() . '/asset/sass/pages/login.scss" />';
+}
+add_action('login_head', 'my_custom_login');
