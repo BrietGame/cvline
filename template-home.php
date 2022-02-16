a78664b81c19b241d776d34de9ea213de8e9e9f6
brietalexis <alexis.briet2003@gmail.com>
2022-02-16 15:39:39 +0100

    :zap: Update de la homePage

--- template-home.php
+++ template-home.php
@@ -1,25 +1,21 @@
 <?php
- get_header();
 /* Template Name: HomePage */
 ?>
-<div class="wrap2">
+<section id="home">
+    <?= get_header() ?>
+    <div class="wrap1">
+        <div class="titre">
+            <h1>Générer votre CV en ligne</h1>
+        </div>
 
-    <div class="titre">
-        <h1>Générer votre CV en ligne</h1>
+        <div class="btnGroup">
+            <a class="btnTransparent" href="#">S'inscrire/Se connecter</a>
+        </div>
+
+        <div class="btnGroup2">
+            <a class="btnWhite" href="#"><i class="fa-solid fa-file-lines"></i>Générez son cv</a>
+            <a class="btnBlue" href="#"><i class="fa-solid fa-user"></i>Espace Recruteur</a>
+        </div>
     </div>
-
-    <div class="btnGroup">
-        <a class="btnTransparent" href="#">S'inscrire/Se connecter</a>
-    </div>
-
-    <div class="btnGroup2">
-
-        <a class="btnWhite" href="#"><i class="fa-solid fa-file-lines"></i>Générez son cv</a>
-        <a class="btnBlue" href="#"><i class="fa-solid fa-user"></i>Espace Recruteur</a>
-    </div>
-</div>
-
-
-<?php
-get_footer();
-?>
+    <?= get_footer() ?>
+</section>
