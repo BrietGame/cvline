f02aff03ed16ed8a7dd82921b56ade3016781dd4
brietalexis <alexis.briet2003@gmail.com>
2022-02-21 14:45:30 +0100

    :zap: Update du header

--- header.php
+++ header.php
@@ -15,21 +15,21 @@
 <html <?php language_attributes(); ?> xmlns="http://www.w3.org/1999/html">
 
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
 
-	<div class="logo">
+    <div class="logo">
 
         <a href="<?php echo path('/'); ?>"><img src=" <?php echo get_template_directory_uri() ?>/asset/img/logoCVline1.svg" alt="logo"></a>
-        <div id="myNav" class="overlay">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <div id="menuprincipal" class="displaynone">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeMenu()">&times;</a>
             <div class="overlay-content">
                 <a class="#" href="<?php echo path('login'); ?>">S'inscrire/Se connecter</a>
                 <a href="<?php echo path('faq'); ?>">FAQ</a>
@@ -38,16 +38,21 @@
                 <a href="<?php echo path('about'); ?>">À propos</a>
             </div>
         </div>
-        <span class="openNav" onclick="openNav()">&#9776;</span>
+        <span class="openNav" onclick="openMenu()">&#9776;</span>
     </div>
+    <script>
+        // HEADER
+        function openMenu() {
+            document.getElementById("menuprincipal").className = "overlay slide-in-left ";
+        }
 
-<script>
-
-    function openNav() {
-        document.getElementById("myNav").style.width = "100%";
-    }
-
-    function closeNav() {
-        document.getElementById("myNav").style.width = "0%";
-    }
-</script>
+        function closeMenu() {
+            document.getElementById("menuprincipal").className = "overlay slide-out-left";
+            setTimeout(function() {
+                document.getElementById("menuprincipal").className = "displaynone";
+            }, 500)
+        }
+        $('#openMenu').on('click', function(e) {
+            openMenu();
+        })
+    </script>
