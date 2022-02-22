a11a4d00fd7f5acbde5244caaa93ba5d770813ab
dormandluc <luc.dormand@hotmail.fr>
2022-02-22 16:04:59 +0100

    création espace recruteur + pagination

--- template-recruteur.php
+++ template-recruteur.php
@@ -9,8 +9,57 @@ get_header();
 <div>
     <h1>EspaceRecruteur</h1>
 </div>
+<section id="recruteur">
+    <input type="text" id="input_recrut" onkeyup="myFunction()" placeholder="Search for names..">
 
+    <ul id="recrutcv">
+        <li><a href="#">Adele</a></li>
+        <li><a href="#">Agnes</a></li>
 
+        <li><a href="#">Billy</a></li>
+        <li><a href="#">Bob</a></li>
+
+        <li><a href="#">Calvin</a></li>
+        <li><a href="#">Christina</a></li>
+        <li><a href="#">Cindy</a></li>
+    </ul>
+
+</section>
+<section id="pagination_recrut">
+
+    <div class="center">
+        <div class="pagination">
+            <a href="#">&laquo;</a>
+            <a href="#">1</a>
+            <a href="#" class="active">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <a href="#">5</a>
+            <a href="#">6</a>
+            <a href="#">&raquo;</a>
+        </div>
+    </div>
+</section>
+    <script>
+        function myFunction() {
+            // Declare variables
+            var input, filter, ul, li, a, i, txtValue;
+            input = document.getElementById('input_recrut');
+            filter = input.value.toUpperCase();
+            ul = document.getElementById("recrutcv");
+            li = ul.getElementsByTagName('li');
+
+            // Loop through all list items, and hide those who don't match the search query
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                txtValue = a.textContent || a.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
+        }</script>
 
 <?php
 get_footer();
