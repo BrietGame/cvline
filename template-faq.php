9dc1a1fb7ee1c8155d1155e043875a9653c850bf
anaiscrenier <anais000@hotmail.fr>
2022-02-23 09:09:36 +0100

    recuperation des modification

--- template-faq.php
+++ template-faq.php
@@ -1,13 +1,53 @@
 <?php
 
-/* Template Name: FAQ */
-get_header();
-?>
+/* Template Name: FAQ */?>
 
-<div>
-    <h1>FAQ</h1>
-</div>
+    <?php get_header(); ?>
 
+ <?php
+ $args = array(
+         'post_type' => 'post',
+         'posts_per_page' => 9,
+ );
 
-<?php
-get_footer();
+ $the_query = new WP_Query($args);
+
+ ?>
+    <section id="faq">
+        <?php if ( $the_query->have_posts() ) {?>
+        <button class="accordion">';
+            <?=get_the_title();
+            while ( $the_query->have_posts() ){
+            $the_query->the_post(); ?>
+            <div class="panel">
+                <p><?=get_the_content(); ?></p>
+            }
+
+        }
+
+            </div>
+        </button>
+
+</section>
+
+<script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }</script>
+
+  <?php get_footer();?>
