f9a58ecddbe03a764c973fd6d2ff4f66b6e067af
brietalexis <alexis.briet2003@gmail.com>
2022-02-24 09:21:11 +0100

    :zap: Espace Candidat

--- template-candidat.php
+++ template-candidat.php
@@ -20,20 +20,20 @@ if (!is_user_logged_in()) {
     <div class="wrap3">
         <section id="candidat">
             <div class="search">
-                <input type="text" id="input_candidat" onkeyup="myFunction()" placeholder="Tapez votre recherche">
+                <input type="text" id="input_candidat" onkeyup="searchKeyWordsCandidat()" placeholder="Tapez votre recherche">
             </div>
             <div class="titre">
                 <h1>Résulat de votre recherche : ( mot clés entrés )</h1>
             </div>
-            <div id="conteneurCandidat">
+            <div id="candidatcv">
                 <ul id="CvUser">
-                    <?php for($i=0; $i<=count($getCvInfo)-1; $i++) { ?>
+                    <?php for ($i = 0; $i <= count($getCvInfo) - 1; $i++) { ?>
                         <div class="decritpion_cv_info">
-                        <a href="<?= path('template-cv1').'?id='.$i.'&cv='.$getCvInfo[$i]->id; ?>"><?= $getCvInfo[$i]->cv_title_work ?></a>
-                        <p>De : <?= $getCvInfo[$i]->cv_surname.' '. $getCvInfo[$i]->cv_name ?></p>
-                        <p>Le : <?= $getCvInfo[$i]->cv_created_at ?></p>
-                    </div>
-                   <?php } ?>
+                            <a href="<?= path('template-cv1') . '?id=' . $i . '&cv=' . $getCvInfo[$i]->id; ?>"><?= $getCvInfo[$i]->cv_title_work ?></a>
+                            <p>De : <?= $getCvInfo[$i]->cv_surname . ' ' . $getCvInfo[$i]->cv_name ?></p>
+                            <p>Le : <?= $getCvInfo[$i]->cv_created_at ?></p>
+                        </div>
+                    <?php } ?>
                 </ul>
             </div>
     </div>
