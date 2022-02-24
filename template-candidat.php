20d52f0e2f70ca1f59f24a2d5b3798ba19dac591
brietalexis <alexis.briet2003@gmail.com>
2022-02-24 18:57:04 +0100

    :zap: Update

--- template-candidat.php
+++ template-candidat.php
@@ -4,6 +4,7 @@ isNotLogged();
 deniedAccessRole('Recruteur');
 //debug($getCvInfoGlobal);
 //debug($getCvInfo);
+get_header();
 
 ?>
 
@@ -12,31 +13,36 @@ deniedAccessRole('Recruteur');
 </div>
 <section id="candidat">
     <div class="header">
-        <?= get_header() ?>
         <h2>Espace Candidat</h2>
     </div>
 
-    <div class="wrap3">
-        <section id="candidat">
-            <div class="search">
-                <input type="text" id="input_candidat" onkeyup="searchKeyWordsCandidat()" placeholder="Tapez votre recherche">
-            </div>
-            <div class="titre">
-                <h1>Résulat de votre recherche : ( mot clés entrés )</h1>
-            </div>
-            <div id="candidatcv">
-                <ul id="CvUser">
+    <div class="wrap">
+        <div class="search">
+            <input type="text" id="input_candidat" onkeyup="searchKeyWordsCandidat()" placeholder="Tapez votre recherche">
+        </div>
+        <div class="titre">
+            <h1>Résulat de votre recherche :</h1>
+        </div>
+        <div id="candidatcv">
+            <ul id="CvUser">
+                <?php if (!empty($getCvInfo)) { ?>
                     <?php for ($i = 0; $i <= count($getCvInfo) - 1; $i++) { ?>
                         <div class="decritpion_cv_info">
                             <a href="<?= path('template-cv1') . '?id=' . $i . '&cv=' . $getCvInfo[$i]->id; ?>"><?= $getCvInfo[$i]->cv_title_work ?></a>
-                            <p>De : <?= $getCvInfo[$i]->cv_surname . ' ' . $getCvInfo[$i]->cv_name ?></p>
-                            <p>Le : <?= $getCvInfo[$i]->cv_created_at ?></p>
+                            <p><i class="fas fa-user"></i> : <?= $getCvInfo[$i]->cv_surname . ' ' . $getCvInfo[$i]->cv_name ?></p>
+                            <p><i class="fas fa-calendar"></i> : <?php $dateCreate = date_create($getCvInfo[$i]->cv_created_at);
+                                                                    echo date_format($dateCreate, 'D d F à G:i') ?></p>
                         </div>
-                    <?php } ?>
-                </ul>
-            </div>
+                <?php }
+                } else {
+                    echo '<p>Vous n\'avez pas de CV de généré sur votre espace.</p>
+                        <a class="btnBlue" href="' . path('/generer-cv') . '">Générer un CV</a>
+                    ';
+                }
+                ?>
+            </ul>
+        </div>
     </div>
-
 </section>
 <?php
 get_footer();
--- template-recruteur.php
+++ template-recruteur.php
@@ -10,18 +10,25 @@ get_header();
     <div class="header">
         <h2>Espace Recruteur</h2>
     </div>
-    <input type="text" id="input_recrut" onkeyup="searchKeyWordsRecruteur()" placeholder="Search for names..">
+    <div class="wrap">
+        <input type="text" id="input_recrut" onkeyup="searchKeyWordsRecruteur()" placeholder="Tapez votre recherche">
 
-    <div id="recrutcv">
-        <?php for ($i = 0; $i <= count($getCvRecruteur) - 1; $i++) { ?>
-            <div class="decritpion_cv_info">
-                <a href="<?= path('template-cv1') . '?id=' . $i . '&cv=' . $getCvRecruteur[$i]->id; ?>"><?= $getCvRecruteur[$i]->cv_title_work ?></a>
-                <p><?= $getCvRecruteur[$i]->cv_surname . ' ' . $getCvRecruteur[$i]->cv_name ?></p>
-                <p><?= $getCvRecruteur[$i]->cv_created_at ?></p>
-            </div>
-        <?php } ?>
+        <div id="recrutcv">
+            <?php if (!empty($getCvInfo)) { ?>
+                <?php for ($i = 0; $i <= count($getCvRecruteur) - 1; $i++) { ?>
+                    <div class="decritpion_cv_info">
+                        <a href="<?= path('template-cv1') . '?id=' . $i . '&cv=' . $getCvRecruteur[$i]->id; ?>"><?= $getCvRecruteur[$i]->cv_title_work ?></a>
+                        <p><i class="fas fa-user"></i> : <?= $getCvRecruteur[$i]->cv_surname . ' ' . $getCvRecruteur[$i]->cv_name ?></p>
+                        <p><i class="fas fa-calendar"></i> :<?php $dateCreate = date_create($getCvRecruteur[$i]->cv_created_at);
+                                                            echo date_format($dateCreate, 'D d F à G:i'); ?></p>
+                    </div>
+            <?php }
+            } else {
+                echo '<p>Aucun CV ne correspond à votre recherche, <strong>Veuillez réessayer</strong></p>';
+            }
+            ?>
+        </div>
     </div>
-
 </section>
 
 <?php
