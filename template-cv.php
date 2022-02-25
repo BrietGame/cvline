11418a18ca486528b2c1a7c4f7de0126e3419039
brietalexis <alexis.briet2003@gmail.com>
2022-02-25 10:42:05 +0100

    :zap: Update DateFormat

--- template-cv.php
+++ template-cv.php
@@ -2,7 +2,7 @@
 /* Template Name: cv1 */
 require __DIR__ . '/vendor/autoload.php';
 isNotLogged();
-if( $_GET['id'] <0 || $_GET['cv'] < 0){
+if ($_GET['id'] < 0 || $_GET['cv'] < 0) {
     wp_redirect(path('/?access=denied'));
 }
 
@@ -58,30 +58,32 @@ $singleCvID = $getCvInfo[$_GET['id']]
     }
 </style>
 <div id="cv_2">
-        <div class="left">
-            <div class="about">
-                <img class="img_cv1" src="https://studiobontant.fr/images/galeries/Big/_DSC6716.jpg" alt="Logo Alexis BRIET">
-                <div class="text_left_cv">
-                    <h3><?= $singleCvID->cv_title_work ?></h3>
-                    <span><?= $singleCvID->cv_surname . ' ' . $singleCvID->cv_name ?></span>
-                    <p>Né(e) le : <?php $dateBirthday = date_create($singleCvID->cv_birthday);
-                    echo date_format($dateBirthday, 'D d F') ?></p>
-                    <p>Adresse : <?= $singleCvID->cv_adress ?></p>
-                    <p><?= $singleCvID->cv_postal . ', ' . $singleCvID->cv_city ?></p>
-                    <p>Téléphone : <?= $singleCvID->cv_phone ?></p>
-                    <p>Email : <?= $singleCvID->cv_email ?></p>
-                    <p>Permis B</p>
-                </div>
+    <div class="left">
+        <div class="about">
+            <img class="img_cv1" src="https://studiobontant.fr/images/galeries/Big/_DSC6716.jpg" alt="Logo Alexis BRIET">
+            <div class="text_left_cv">
+                <h3><?= $singleCvID->cv_title_work ?></h3>
+                <span><?= $singleCvID->cv_surname . ' ' . $singleCvID->cv_name ?></span>
+                <p>Né(e) le : <?php $dateBirthday = date_create($singleCvID->cv_birthday);
+                                echo date_format($dateBirthday, 'D d F Y') ?></p>
+                <p>Adresse : <?= $singleCvID->cv_adress ?></p>
+                <p><?= $singleCvID->cv_postal . ', ' . $singleCvID->cv_city ?></p>
+                <p>Téléphone : <?= $singleCvID->cv_phone ?></p>
+                <p>Email : <?= $singleCvID->cv_email ?></p>
+                <p>Permis B</p>
             </div>
-            <p class="aboutmecv"><?= $singleCvID->cv_presentation ?></p>
         </div>
+        <p class="aboutmecv"><?= $singleCvID->cv_presentation ?></p>
+    </div>
 
     <div class="right">
         <h1>Mon expérience</h1>
         <?php for ($i = 0; $i <= count($getCvWork) - 1; $i++) { ?>
             <div class="experience">
                 <h2><?= $getCvWork[$i]->jobname ?></h2>
-                <span><?= $getCvWork[$i]->jobcompany . ' | ' . $getCvWork[$i]->jobstart . ' - ' . $getCvWork[$i]->jobend ?></span>
+                <?php $datejobStart = date_create($getCvWork[$i]->jobstart); ?>
+                <?php $datejobEnd = date_create($getCvWork[$i]->jobstart); ?>
+                <span><?= $getCvWork[$i]->jobcompany . ' | ' . date_format($datejobStart, 'F Y') . ' - ' . date_format($datejobStart, 'F Y') ?></span>
                 <p class="description_post"><?= $getCvWork[$i]->jobdescription ?></p>
 
             </div>
@@ -91,7 +93,9 @@ $singleCvID = $getCvInfo[$_GET['id']]
         <?php for ($i = 0; $i <= count($getCvSchool) - 1; $i++) { ?>
             <div class="parcours">
                 <h2><?= $getCvSchool[$i]->schooljob ?></h2>
-                <span><?= $getCvSchool[$i]->schoolname . ' | ' . $getCvSchool[$i]->schoolstart . ' - ' . $getCvSchool[$i]->schoolend ?></span>
+                <?php $dateschoolStart = date_create($getCvSchool[$i]->schoolstart); ?>
+                <?php $dateschoolEnd = date_create($getCvSchool[$i]->schoolend); ?>
+                <span><?= $getCvSchool[$i]->schoolname . ' | ' . date_format($datejobStart, 'F Y') . ' - ' . date_format($dateschoolEnd, 'F Y') ?></span>
                 <p><?= $getCvSchool[$i]->schoolplace ?></p>
                 <p><?= $getCvSchool[$i]->schooldescription ?></p>
             </div>
